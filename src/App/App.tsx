a21e6d61c7c7f6a8aec22e22d5e490a659c04e78
Denys <denisskorinov41@gmail.com>
2024-07-10 14:52:07 +0300

    Added pages

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,15 @@
 import socketIO from 'socket.io-client'
 import { Route, Routes } from 'react-router-dom'
+import Chat from '../pages/chat/chat';
 import './App.css'
 const socket = socketIO('http://localhost:5000');
 
 function App() {
   return (
     <Routes>
-      <Route path='/'></Route>
-      <Route path='/chat'></Route>
+      <Route path='/' element={<Chat />}></Route>
+      {/* <Route path='/login'></Route>
+      <Route path='/signup'></Route> */}
     </Routes>
   )
 }
---
+++ src/pages/chat/chat.tsx
@@ -0,0 +1,14 @@
+import React from "react"
+import SideBar from "../../components/sideBar/sideBar"
+import Header from "../../components/header/header"
+
+function Chat() {
+  return(
+    <>
+      <Header />
+      <SideBar />
+    </>
+  )
+}
+
+export default Chat
