5e7ee718318f3d26a55e037d696fc68c8e9d368d
Denys <denisskorinov41@gmail.com>
2024-07-10 18:07:28 +0300

    Fixed layout

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,11 +1,16 @@
+import './main.css'
 import SideBar from "../../components/sideBar/sideBar"
 import Header from "../../components/header/header"
 import Chat from '../../components/chat/chat';
+import { useParams } from 'react-router-dom';
 
 function Main() {
+  const id = useParams();
+  console.log(id);
+
   return(
     <>
-      {/* <SideBar /> */}
+      <SideBar />
       <div className='wrapper'>
         <Header />
         <Chat />
