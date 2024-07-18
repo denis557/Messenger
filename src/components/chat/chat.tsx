059d8598e33cee6c875b08e03186e6f3e305baeb
denis557 <denisskorinov41@gmail.com>
2024-07-18 03:44:24 +0300

    Started message component

--- src/components/chat/chat.tsx
+++ src/components/chat/chat.tsx
@@ -1,10 +1,39 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './chat.css'
+import { useSelector } from 'react-redux';
 
 function Chat() {
+    const { selectedUser } = useSelector(state => state.user);
+    const [messages, setMessages] = useState([]);
+    const [loadingMessages, setLoadingMessages] = useState(true);
+
+    useEffect(() => {
+        if(selectedUser.userId) {
+            const getMessages = async () => {
+                try {
+                    const res = await fetch(`/api/message/${selectedUser.userId}`);
+                    const data = await res.json();
+                    if(data.error) {
+                        console.log(data.message);
+                        return
+                    }
+                    setMessages(data.messages);
+                } catch (error) {
+                    console.log(error);
+                } finally {
+                    setLoadingMessages(false);
+                }
+            };
+    
+            getMessages();
+        }
+    }, [selectedUser.userId])
+
     return(
         <div className="chat">
-            
+            {selectedUser._id &&
+                loadingMessages ? <p>Loading...</p> : messages.map((message, index) => <p key={index}>{message.text}</p>)
+            }
         </div>
     )
 }
--- src/components/user/toggleUSer.ts
+++ src/components/user/toggleUSer.ts
@@ -1,13 +1,33 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
-export const userSlice = createSlice({
-    name: 'user',
-    initialState: {
+export interface userInterface {
+    selectedUser: {
+        _id: string,
+        userId: string,
+        username: string
+    }
+};
+
+const initialState: userInterface = {
+    selectedUser: {
         _id: '',
         userId: '',
         username: ''
-    },
-    reducers: {
-        
     }
-})
+}
+
+export const userSlice = createSlice({
+    name: 'user',
+    initialState,
+    reducers: {
+        selectUser: (state, action: PayloadAction<userInterface>) => {
+            state.selectedUser._id = action.payload.selectedUser._id;
+            state.selectedUser.userId = action.payload.selectedUser.userId;
+            state.selectedUser.username = action.payload.selectedUser.username
+        }
+    }
+});
+
+export const { selectUser } = userSlice.actions
+
+export default userSlice.reducer
