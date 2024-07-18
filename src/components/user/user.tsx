0bbc814be70594d01661a72bcfbaee0b060df2a1
denis557 <denisskorinov41@gmail.com>
2024-07-18 03:43:54 +0300

    Configured Redux

--- src/components/user/user.tsx
+++ src/components/user/user.tsx
@@ -1,10 +1,14 @@
 import './user.css'
 import { firstLetter } from '../../helpers/firstLetter'
 import { changeTimeZone } from '../../helpers/changeTimeZone'
+import { useDispatch, useSelector } from 'react-redux'
+import { selectUser } from './toggleUser';
 
 function User({ chat }) {
+  const { selectedUser } = useSelector(state => state.user);
+  const dispatch = useDispatch();
   return(
-    <div className='user'>
+    <div className={`user ${selectedUser._id === chat._id && 'active'}`} onClick={() => dispatch(selectUser({ selectedUser: {_id: chat._id, userId: chat.members[0]._id, username: chat.members[0].name} }))}>
       <div className='sideBar_user_avatar'>{chat.members[0].avatar ? chat.members[0].avatar : firstLetter(chat.members[0].name)}</div>
       <div className='user_text'>
         <p className='user_name'>{chat.members[0].name}</p>
--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -7,11 +7,6 @@ import { useEffect, useState } from 'react';
 function Main() {
   const [loadingChats, setLoadingChats] = useState(true);
   const [chats, setChats] = useState(null);
-  // const [selectedChat, setSelectedChat] = useState({
-  //   _id: '',
-  //   userId: '',
-  //   username: ''
-  // })
 
   useEffect(() => {
     const getChats = async () => {
